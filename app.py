c6234fbb1e368004b89b40c825199ed284097cdf
Hithesh Jay <hithesh0215@gmail.com>
2023-10-01 18:09:14 +0530

    added a log file

--- app.py
+++ app.py
@@ -4,35 +4,46 @@ from predict import detect_emotions
 import os
 from PIL import Image
 import cv2
+import logging
+
+# Set up logging
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST','GET'])
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
+    # Handle file upload
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         # Save the uploaded file
         file_path = os.path.join('static', 'uploads', uploaded_file.filename)
         uploaded_file.save(file_path)
 
-        img = cv2.imread(file_path)
-        # Predict emotion and add bounding box and return the image
-        emotion_img = detect_emotions(img)
-        output_path = os.path.join('static', 'output', uploaded_file.filename)
-        
-        emotion_img = cv2.cvtColor(emotion_img, cv2.COLOR_BGR2RGB)
-        emotion_img = Image.fromarray(emotion_img)
-        
-        emotion_img.save(output_path , mimetype='image/png')
+        try:
+            # Predict emotion and add bounding box
+            img = cv2.imread(file_path)
+            emotion_img = detect_emotions(img)
+            output_path = os.path.join('static', 'output', uploaded_file.filename)
+            emotion_img = cv2.cvtColor(emotion_img, cv2.COLOR_BGR2RGB)
+            emotion_img = Image.fromarray(emotion_img)
+            emotion_img.save(output_path)
+            # Log success message
+            logging.info(f"File {uploaded_file.filename} processed successfully")
 
+        except Exception as e:
+            # Log error message
+            logging.error(f"An error occurred while processing file {uploaded_file.filename}: {e}")
+            
+        # Pass the filename of the processed image to the index.html file
         return render_template('index.html', image=uploaded_file.filename)
-    
+
     # else
     return 'Error uploading file'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
