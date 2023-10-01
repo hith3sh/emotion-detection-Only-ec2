d4bc8b57a5771c1505a6a81e69ac7f50af431f12
Hithesh Jay <hithesh0215@gmail.com>
2023-10-01 16:42:21 +0530

    fixed upload route to GET & POST method

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST','GET'])
 def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
